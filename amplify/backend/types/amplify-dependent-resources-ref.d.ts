3d7a549c203321b2eee61a124eb74112e4a15e0c
Hiwot <hiwotbishaw@gmail.com>
2023-08-27 15:19:22 -0500

    v2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,41 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "adv": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "adve0427cd7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "adv": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "adv": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,128 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { API } from "aws-amplify";
+import {
+  withAuthenticator,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View
+} from "@aws-amplify/ui-react";
+import { listTodos } from "./graphql/queries";
+import {
+  createTodo as createNoteMutation,
+  deleteTodo as deleteNoteMutation,
+} from "./graphql/mutations";
 
-function App() {
+function App({ signOut }) {
+  const [notes, setNotes] = useState([]);
+ 
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  if(window.location.href.indexOf("test") != -1){
+    console.log(notes);
+    let longUrl = notes.filter(n=> n.description == window.location.href)
+   
+    console.log(longUrl, window.location.href)
+    
+    if(longUrl.length && longUrl[0].name){
+      // console.log(longUrl.length , longUrl[0].name)
+
+      // eslint-disable-next-line no-restricted-globals
+      location.assign(longUrl[0].name);
+    }
+    else
+    return <h1>wrong url</h1>
+
+  }
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos });
+    const notesFromAPI = apiData.data.listTodos.items;
+    setNotes(notesFromAPI);
+  }
+  const idToShortURL = () => {
+    let n = Math.floor(Math.random() * (10000 - 1 + 1) + 1)
+    let map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let shorturl = [];
+    while (n) 
+    {
+        shorturl.push(map[n % 62]);
+        n = Math.floor(n / 62);
+    }
+    shorturl.reverse();
+    
+    return shorturl.join("");
+  }
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      name: form.get("name"),
+      description: window.location.href + 'test/' + idToShortURL(),
+    };
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id }) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        AdV
-      </header>
-    </div>
+    <View className="App">
+      <Heading level={1}>AdV</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Url"
+            label="URL"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Post
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Posts</Heading>
+      <View margin="3rem 0">
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.description}
+            </Text>
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete post
+            </Button>
+          </Flex>
+        ))}
+      </View>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
